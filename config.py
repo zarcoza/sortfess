1f22a2a266b9868b8ddfbaa85219acfe44e847c7
Zara <azaleazaragoza@gmail.com>
2025-05-16 10:28:21 +0700

    config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 BOT_TOKEN = '7685483291:AAHWaSSCBdCU1ZT0BFeoPHEyMIEPJJvzVDw'
 CHANNEL_ID = '@sortfess'   # ganti dengan username channel menfess kamu
-ADMIN_ID = 1538087933, 7608777733                  # ganti dengan Telegram ID admin
+ADMIN_IDS = {1538087933, 7608777733} # ganti dengan Telegram ID admin
 REQUIRED_CHANNELS = ['@sortfess', '@fiIIyourheart']
 
 # Daftar hashtag yang valid untuk menfess
